f218b70e17bfdf87e576bfe0fbde7740bb8960d2
dev-mich <michelongiacomo@gmail.com>
2019-03-14 09:26:57 +0100

    Add scope validation middleware and options

---
+++ AspNetCore.ScopeValidation/ScopeValidationMiddleware.cs
@@ -0,0 +1,119 @@
+﻿using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspNetCore.ScopeValidation
+{
+    public class ScopeValidationMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly ScopeValidationOptions _options;
+        private readonly ILogger<ScopeValidationMiddleware> _logger;
+
+        public ScopeValidationMiddleware(RequestDelegate next, IOptions<ScopeValidationOptions> options, ILogger<ScopeValidationMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+            _options = options.Value;
+        }
+
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            _logger.LogDebug($"Begin scope validation for action: {context.Request.Path}");
+
+            // get request method and path
+            var requestMethod = context.Request.Method;
+            var requestPath = context.Request.Path;
+
+
+            // ensure that path is not not anonymous
+            if (_options.AnonymousRoutes.Any(r => r.StartsWith(requestPath)))
+            {
+                _logger.LogDebug("Validation skipped cause route allow anonymous users");
+
+                await _next(context);
+
+                return;
+            }
+
+
+
+            // ensure user is authenticated cause route is not anonymous
+
+            AuthenticateResult result = await context.AuthenticateAsync(_options.AuthenticationScheme);
+
+            if (!result.Succeeded)
+            {
+                _logger.LogWarning("User is anonymous, return 401");
+                Unauthorized(context);
+                return;
+            }
+
+
+
+            if (!Validate(requestPath, requestMethod, result.Principal))
+            {
+                _logger.LogWarning("Scope validation failed. Return 403");
+                await Forbidden(context);
+                return;
+            }
+
+
+
+            // validation is ok, go on
+            _logger.LogDebug("Scope validation success");
+
+            await _next(context);
+
+
+
+        }
+
+
+
+
+
+
+        private bool Validate(string path, string method, ClaimsPrincipal principal)
+        {
+            var scopeClaims = principal.FindAll(_options.ScopeClaimType);
+            _logger.LogInformation("Scopes found on principal: {scopes}", scopeClaims);
+
+            if (scopeClaims == null || !scopeClaims.Any())
+
+                return false;
+
+            var grantedScopes = scopeClaims.Select(s => s.Value);
+
+            var allowedScopes = _options.ScopeSchemes.Where(s =>path.StartsWith(s.PathTemplate) && s.RequestMethod.Method.Equals(method)).SelectMany(s => s.AllowedScopes);
+
+            var intersection = allowedScopes.Intersect(grantedScopes);
+
+            return intersection.Any();
+        }
+
+
+
+        public void Unauthorized(HttpContext context)
+        {
+            context.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
+        }
+
+
+
+        public async Task Forbidden(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            await context.Response.WriteAsync($"invalid scopes");
+        }
+    }
+}
---
+++ AspNetCore.ScopeValidation/ScopeValidationOptions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace AspNetCore.ScopeValidation
+{
+    public class ScopeValidationOptions
+    {
+        /// <summary>
+        /// default Bearer
+        /// </summary>
+        public string AuthenticationScheme { get; set; }
+
+
+        /// <summary>
+        /// default scope
+        /// </summary>
+        public string ScopeClaimType { get; set; }
+
+
+        /// <summary>
+        /// List of scope validation rules
+        /// </summary>
+        public List<Scope> ScopeSchemes { get; set; }
+
+
+        /// <summary>
+        /// Routes that are not affected to scope validation, (api/v1 for example skip validation for all routes that starts with this template, pay attention)
+        /// </summary>
+        public List<string> AnonymousRoutes { get; set; }
+
+
+        public ScopeValidationOptions()
+        {
+            if (ScopeSchemes == null)
+
+                throw new ArgumentNullException(nameof(ScopeSchemes));
+
+        }
+
+    }
+
+
+    public class Scope
+    {
+
+        /// <summary>
+        /// If set different paths can have different required scopes, if not set if for all paths that match request method and is not anonymous
+        /// </summary>
+        public string PathTemplate { get; set; }
+
+
+        /// <summary>
+        /// Associated scope request method
+        /// </summary>
+        public HttpMethod RequestMethod { get; set; }
+
+
+        /// <summary>
+        /// List of valid scopes
+        /// </summary>
+        public IEnumerable<string> AllowedScopes { get; set; }
+
+
+        public Scope()
+        {
+            if(AllowedScopes == null)
+
+                    throw new ArgumentNullException(nameof(AllowedScopes));
+
+            if(!AllowedScopes.Any())
+
+                    throw new InvalidOperationException(nameof(AllowedScopes));
+
+            PathTemplate = string.Empty;
+        }
+
+    }
+}
